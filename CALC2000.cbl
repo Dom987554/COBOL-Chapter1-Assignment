692614c470428c5fb10e1d030494f24b568db99a
Dom987554 <123317739+Dom987554@users.noreply.github.com>
2026-01-29 14:28:12 -0600

    Update CALC2000.cbl
    Added all documentation

--- CALC2000.cbl
+++ CALC2000.cbl
@@ -4,9 +4,10 @@
       *  Programmer.:Dominic Mattern
       *  Date.......: 2026.01.28
       *  GitHub URL.:
-      *
-      *  Description:
-      *
+      *  https://github.com/Dom987554/COBOL-Chapter1-Assignment.git
+      *  Description: This program colculates future values for an
+      *  investment and double the investment amount twice.
+
        environment division.
 
        input-output section.
@@ -17,12 +18,21 @@
 
        working-storage section.
 
+      *The values that are initilized at the start.
+      *investment-amount: starting value put in the investment.
+      *number-of-years: amount many years does the interest apply.
+      *yearly-interest-rate: applied interest each year.
        01  input-values.
 
            05  investment-amount           pic 9999    value 1000.
            05  number-of-years             pic 99       value 10.
            05  yearly-interest-rate        pic 9v9     value 5.5.
 
+      *The fields that only get edited during runtime.
+      *future-value: Holds the current money amount through the years.
+      *year-counter: Counts how many years have been calculated out.
+      *display-interest-rate: used only to display the rate at the end.
+      *edited-decimal-value: stores the final future value for display.
        01  work-fields.
 
            05  future-value                pic 9(7)v99.
@@ -33,6 +43,9 @@
 
        procedure division.
 
+      *Formats the start of display output then calculates the
+      *investment for 3 years with the inital value being doubled
+      *each time.
        000-calculate-future-values.
            display "  Calculating Future Values  ".
            perform 100-calculate-future-value
@@ -43,6 +56,8 @@
            display "End of session.".
            stop run.
 
+      *calculates the amount of money there would be if left for 10 
+      *years.
        100-calculate-future-value.
 
            move investment-amount to future-value
@@ -52,6 +67,8 @@
            move future-value to edited-decimal-value
            perform 140-display-values.
 
+      *calculates each next year until 10 years have been found then
+      *returns the 10 year value.
        120-calculate-next-fv.
 
            compute future-value rounded =
@@ -59,6 +76,8 @@
                    (future-value * yearly-interest-rate / 100).
            add 1 to year-counter.
 
+      *displays the investment amount, number of years, yearly
+      *interest, and value after 10 years.
        140-display-values.
            display "- - - - - - - - - - - - - - -"
            display "Investment Amount =      " investment-amount
